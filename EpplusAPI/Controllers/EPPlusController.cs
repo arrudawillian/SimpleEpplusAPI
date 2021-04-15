28f7b9dd5dc1ff87ffe92e549b6a987022388805
Willian Arruda <willianarruda.gft@sulamerica.com.br>
2021-04-15 17:19:22 -0300

    Add project files.

---
+++ EpplusAPI/Controllers/EPPlusController.cs
@@ -0,0 +1,111 @@
+﻿using EpplusAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EpplusAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EPPlusController : ControllerBase
+    {
+        /// <summary>
+        /// Importação
+        /// </summary>
+        /// <param name="formFile"></param>
+        /// <returns></returns>
+        [HttpPost("import")]
+        public DemoResponse<List<UserInfo>> Import(IFormFile formFile)
+        {
+            if (formFile == null || formFile.Length <= 0)
+            {
+                return DemoResponse<List<UserInfo>>.GetResult(-1, "formfile is empty");
+            }
+
+            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                return DemoResponse<List<UserInfo>>.GetResult(-1, "Not Support file extension");
+            }
+
+            var list = new List<UserInfo>();
+
+            using (var stream = new MemoryStream())
+            {
+                formFile.CopyTo(stream);
+
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0]; // ou ExcelWorksheet workSheet = package.Workbook.Worksheets["Customer"];
+                    var rowCount = worksheet.Dimension.Rows;
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        list.Add(new UserInfo
+                        {
+                            UserName = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                            Age = int.Parse(worksheet.Cells[row, 2].Value.ToString().Trim()),
+                        });
+                    }
+                }
+            }
+
+            // add list to db ..  
+            // here just read and return  
+
+            return DemoResponse<List<UserInfo>>.GetResult(0, "OK", list);
+        }
+
+        /// <summary>
+        /// Importação Assincrona
+        /// </summary>
+        /// <param name="formFile"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpPost("importAsync")]
+        public async Task<DemoResponse<List<UserInfo>>> ImportAsync(IFormFile formFile, CancellationToken cancellationToken)
+        {
+            if (formFile == null || formFile.Length <= 0)
+            {
+                return DemoResponse<List<UserInfo>>.GetResult(-1, "formfile is empty");
+            }
+
+            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                return DemoResponse<List<UserInfo>>.GetResult(-1, "Not Support file extension");
+            }
+
+            var list = new List<UserInfo>();
+
+            using (var stream = new MemoryStream())
+            {
+                await formFile.CopyToAsync(stream, cancellationToken);
+
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                    var rowCount = worksheet.Dimension.Rows;
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        list.Add(new UserInfo
+                        {
+                            UserName = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                            Age = int.Parse(worksheet.Cells[row, 2].Value.ToString().Trim()),
+                        });
+                    }
+                }
+            }
+
+            // add list to db ..  
+            // here just read and return  
+
+            return DemoResponse<List<UserInfo>>.GetResult(0, "OK", list);
+        }
+    }
+}
